677f4b6df37cd67f29943479a1b3ebc67fd93319
John MacKintosh <johnmackintosh@users.noreply.github.com>
2024-12-26 21:56:45 +0000

    Update utilities.R

--- utilities.R
+++ utilities.R
@@ -25,3 +25,24 @@ view_results <- function(res_t){
                  "result", "error", .simplify = TRUE) %>%
     View()
 }
+
+# function to join values from multiple rows to one string and perform the necessary unlisting and unnnaming
+combine_rows <- function(x, collapse_sym){
+    x <- paste(x, collapse = collapse_sym) |>
+      unlist() |>
+      unname()
+    return(x)
+  }
+
+
+
+# get start position of first record in each file
+  get_start_positions <- function(x, start_target) {
+    out <- scan(x, nlines = 10,
+         skip = 3, # always skip first 3 rows - in retrospect would have been easier not to skip any rows and have no headers
+         what = "", # character
+         sep = "\n",
+         blank.lines.skip = TRUE)
+ res <-  grep(start_target, out)
+ return(res)
+  }
